e854313e447c188be3c4c218d29a73b291330878
Adam Watt <awattny@me.com>
2017-08-15 09:02:36 -0700

    implement initial app with homepage route - tests passing

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
+var slug = require('shortid');
+var mongoose = require('mongoose');
+var config = require('./config');
+
+var Url = require('./urlModel');
+
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name, {
+  useMongoClient: true,
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+  res.send('Welcome to Shortly');
+});
+
 
 app.listen(process.env.PORT || 4000);
