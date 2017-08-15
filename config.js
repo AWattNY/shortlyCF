10689facedc696600dfb82c66cb092996efc1bee
Adam Watt <awattny@me.com>
2017-08-14 20:54:25 -0700

    implement config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const host = 'http://localhost:4000/';
+
+const dbHost = 'localhost';
+const dbName = 'shortly';
+
+module.exports = { host, 
+  db: { 
+    host: dbHost, 
+    name: dbName 
+  } 
+};
